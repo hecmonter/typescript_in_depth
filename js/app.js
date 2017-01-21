0c9b44619d950d541ee6ccfbdff0e23f115af07b
Patricia Castillo <montero.hector@gmail.com>
2017-01-21 12:29:55 +0100

    feat(app): adds examples about mixins classes funcionalities

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 "use strict";
+var enums_1 = require("./enums");
+var classes_1 = require("./classes");
 var util = require("./lib/utilityFunctions");
 function PrintBookInfo(item) {
     console.log(item.title + " was authored by " + item.author);
@@ -10,8 +12,38 @@ function LogFavorityBooks(_a) {
     PrintBookInfo(book2);
     console.log(others);
 }
-LogFavorityBooks(util.GetAllBooks());
+//LogFavorityBooks(util.GetAllBooks());
 //Tuple types examples: 
 var catalogLocation = ['123.45678', book1];
 var catalogLocation2 = ['other string', book1, 'string', book1];
+// union types: specify several valid types for a value. Vertical bar is used to separate valid types.
+var allBooks = util.GetAllBooks();
+var allMagazines = util.GetAllMagazines();
+var readingMaterial = allBooks[0];
+function PrintTitle(item) {
+    console.log(item.title);
+}
+// PrintTitle(allBooks[0]);
+// PrintTitle(allMagazines[0]); 
+// interception types examples: 
+var serialNovel = {
+    id: 100,
+    title: 'The Gradual Tale',
+    author: 'Occasional Pen',
+    available: true,
+    category: enums_1.Category.Fiction,
+    publisher: 'Serial Press' // Magazine property    
+};
+// mixins 
+function applyMixins(derivedCtor, baseCtors) {
+    baseCtors.forEach(function (baseCtor) {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(function (name) {
+            derivedCtor.prototype[name] = baseCtor.prototype[name];
+        });
+    });
+}
+applyMixins(classes_1.UniversityLibrarian, [classes_1.Employee, classes_1.Researcher]);
+var newLibrarianClass = new classes_1.UniversityLibrarian();
+newLibrarianClass.doResearch('Economics');
+//console.log('%O', UniversityLibrarian); 
 //# sourceMappingURL=app.js.map
